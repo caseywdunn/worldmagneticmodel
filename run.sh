ec661738852660d72956d0d1ac57d320cfa1f5d8
Casey Dunn <casey.dunn@yale.edu>
2024-12-25 18:25:04 -0500

    housekeeping

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd src
-if clang -g -fsanitize=address main.c GeomagnetismLibrary.c -o main; then
+if clang -g -fsanitize=address main.c GeomagnetismLibrary.c -o magpoint; then
     cd ..
-    ./src/main 30.0 -70.0 400.0 2025.0
+    ./src/magpoint 30.0 -70.0 400.0 2025.0
 else
     cd ..
 fi
